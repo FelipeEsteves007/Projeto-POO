a8bfe1594088f3d93862a346e067b710ebe96d9f
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-06-10 13:56:31 -0300

    add netbeans

---
+++ POO-Netbeans/Programador.java
@@ -0,0 +1,47 @@
+// Felipe Esteves de Paula
+// RA: 2389576
+
+// Classe mãe
+public abstract class Programador implements Exibivel {
+    private String nome;
+    private boolean ocupado = false;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+    
+    // Método abstrato
+    public abstract String getTipo();
+
+    //Sobrecarga
+    public void mostrarDetalhes() {
+        System.out.println("Nome: " + getNome());
+    }
+
+    //Sobrecarga
+    public void mostrarDetalhes(String projeto) {
+        System.out.println("Nome: " + getNome() + " | Projeto: " + projeto);
+    }
+
+    //Sobrecarga
+    public void mostrarDetalhes(String projeto, int horas) {
+        System.out.println("Nome: " + getNome() + " | Projeto: " + projeto + " | Horas: " + horas);
+    }
+
+    //Interface
+    public void exibirDados() {
+        System.out.println("Programador: " + getNome() + " | Tipo: " + getTipo() + " | Ocupado: " + (ocupado ? "Sim" : "Não"));
+    }
+}
